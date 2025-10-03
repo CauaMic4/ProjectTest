a9c75103184fa1eb62bd9e07fb04da5ecd6ed99f
Cauã Micael <cauamicael@hotmail.com>
2025-10-03 13:39:08 -0300

    serialization

--- ProjectTest/ProjectTest/Data/VO/BookVO.cs
+++ ProjectTest/ProjectTest/Data/VO/BookVO.cs
@@ -1,9 +1,16 @@
-﻿namespace ProjectTest.Data.VO
+﻿using System.Text.Json.Serialization;
+
+namespace ProjectTest.Data.VO
 {
     public class BookVO
     {
+        [JsonPropertyName("code")]
         public long Id { get; set; }
+
+        [JsonPropertyName("author")]
         public string Author { get; set; }
+
+        [JsonPropertyName("title")]
         public string Title { get; set; }
         public DateTime Launch_date { get; set; }
         public decimal Price { get; set; }
--- ProjectTest/ProjectTest/Data/VO/PersonVO.cs
+++ ProjectTest/ProjectTest/Data/VO/PersonVO.cs
@@ -1,11 +1,22 @@
-﻿namespace ProjectTest.Data.VO
+﻿using System.Text.Json.Serialization;
+
+namespace ProjectTest.Data.VO
 {
     public class PersonVO
     {
+        [JsonPropertyName("code")]
         public long Id { get; set; }
-        public string Address { get; set; }
+
+        [JsonPropertyName("name")]
         public string FirstName { get; set; }
+
+        [JsonPropertyName("last_name")]
         public string LastName { get; set; }
+
+        [JsonIgnore]
+        public string Address { get; set; }
+
+        [JsonPropertyName("sex")]
         public string Gender { get; set; }
     }
 }
