f997ef08f1f69260aefe7c404b5118a7320c64f4
Cauã Micael <cauamicael@hotmail.com>
2025-10-17 23:41:37 -0300

    adding new implementations for data paging

---
+++ ProjectTest/ProjectTest/Hypermedia/Utils/PagesSearchVO.cs
@@ -0,0 +1,50 @@
+﻿using ProjectTest.Hypermedia.Abstract;
+
+namespace ProjectTest.Hypermedia.Utils
+{
+    public class PagesSearchVO<T> where T : ISupportsHyperMedia
+    {
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public long TotalResults { get; set; }
+        public string SortFields { get; set; }
+        public string SortDirections { get; set; }
+        public Dictionary<string, object> Filters { get; set; }
+        public List<T> List { get; set; }
+
+        public PagesSearchVO()
+        {
+        }
+
+        public PagesSearchVO(int currentPage, int pageSize, string sortFields, string sortDirections)
+        {
+            CurrentPage = currentPage;
+            PageSize = pageSize;
+            SortFields = sortFields;
+            SortDirections = sortDirections;
+        }
+
+        public PagesSearchVO(int currentPage, int pageSize, string sortFields, string sortDirections, Dictionary<string, object> filters) : this(currentPage, pageSize, sortFields, sortDirections)
+        {
+            CurrentPage = currentPage;
+            PageSize = pageSize;
+            SortFields = sortFields;
+            SortDirections = sortDirections;
+            Filters = filters;
+        }
+
+        public PagesSearchVO(int currentPage, string sortFields, string sortDirections) : this(currentPage, 10, sortFields, sortDirections)
+        {
+        }
+
+        public int GetCurrentPage()
+        {
+            return CurrentPage == 0 ? 2 : CurrentPage;
+        }
+
+        public int GetPageSize()
+        {
+            return PageSize == 0 ? 10 : PageSize;
+        }
+    }
+}
