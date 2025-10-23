d73d9e3dced4d0851bf0adddef4f78a13a2ff3a2
Cauã Micael <cauamicael@hotmail.com>
2025-10-22 22:26:17 -0300

    implementation for file uploads

---
+++ ProjectTest/ProjectTest/Business/IFileBusiness.cs
@@ -0,0 +1,12 @@
+﻿using ProjectTest.Data.VO;
+
+namespace ProjectTest.Business
+{
+    public interface IFileBusiness
+    {
+        public byte[] GetFile(string filename);
+        public Task<FileDetailVO> SaveFileToDisk(IFormFile file);
+        public Task<List<FileDetailVO>> SaveFilesToDisk(IList<IFormFile> file);
+
+    }
+}
---
+++ ProjectTest/ProjectTest/Business/Implementations/FileBusiness.cs
@@ -0,0 +1,31 @@
+﻿using ProjectTest.Data.VO;
+
+namespace ProjectTest.Business.Implementations
+{
+    public class FileBusiness : IFileBusiness
+    {
+        private readonly string _basePath;
+        private readonly IHttpContextAccessor _context;
+
+        public FileBusiness(IHttpContextAccessor context)
+        {
+            _context = context;
+            _basePath = Directory.GetCurrentDirectory() + "\\UploadsDir\\";
+        }
+
+        public byte[] GetFile(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<FileDetailVO>> SaveFilesToDisk(IList<IFormFile> file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FileDetailVO> SaveFileToDisk(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
